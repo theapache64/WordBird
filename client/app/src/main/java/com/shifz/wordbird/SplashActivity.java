36a6c280b2d27f3d925218c851d0b519067bc7fa
theapache64-oscar <theapache64@gmail.com>
2016-11-12 15:02:21 +0530

    Added marshmallow support

--- client/app/src/main/java/com/shifz/wordbird/SplashActivity.java
+++ client/app/src/main/java/com/shifz/wordbird/SplashActivity.java
@@ -1,13 +1,12 @@
 package com.shifz.wordbird;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 import com.shifz.wordbird.utils.App;
@@ -15,11 +14,36 @@ import com.shifz.wordbird.utils.App;
 public class SplashActivity extends AppCompatActivity {
 
     private static final long SPLASH_DELAY = 3500;
+    private static final int RQ_CODE_RQ_WRITE_EXTERNAL_STORAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (checkSelfPermission(Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
+
+                if (shouldShowRequestPermissionRationale(Manifest.permission.GET_ACCOUNTS)) {
+                    requestPermissions(new String[]{Manifest.permission.GET_ACCOUNTS, Manifest.permission.READ_CONTACTS, Manifest.permission.READ_PHONE_STATE}, RQ_CODE_RQ_WRITE_EXTERNAL_STORAGE);
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.GET_ACCOUNTS, Manifest.permission.READ_CONTACTS, Manifest.permission.READ_PHONE_STATE}, RQ_CODE_RQ_WRITE_EXTERNAL_STORAGE);
+                }
+
+            } else {
+                doNormalSplashWork();
+            }
+
+        } else {
+            doNormalSplashWork();
+        }
+
+
+    }
+
+    private void doNormalSplashWork() {
+
         ((TextView) findViewById(R.id.tvAppVersion)).setText(String.format("v%s", BuildConfig.VERSION_NAME));
 
         //Checking if the api key exists
@@ -43,6 +67,8 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(nextActivityIntent);
             }
         }, SPLASH_DELAY);
+
+
     }
 
     @Override
