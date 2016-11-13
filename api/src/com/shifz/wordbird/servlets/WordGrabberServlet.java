f5198f4f3bc66dbce27181d0326b11c54e2f8591
theapache64@charlie <theapache64@gmail.com>
2016-11-13 18:35:30 +0530

    Got to build grabber

---
+++ api/src/com/shifz/wordbird/servlets/WordGrabberServlet.java
@@ -0,0 +1,9 @@
+package com.shifz.wordbird.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by theapache64 on 13/11/16.
+ */
+public class WordGrabberServlet extends HttpServlet {
+}
